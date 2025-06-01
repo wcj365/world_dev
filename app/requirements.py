6896b187aabdaff830e38cc65cc4352a35eec4dc
Dr. Chaojie Wang <wcj365@gmail.com>
2025-06-01 09:12:56 -0400

    Update requirements.py

--- app/requirements.py
+++ app/requirements.py
@@ -3,5 +3,5 @@ plotly
 pandas
 openpyxl
 xlsxwriter
-git+https://gitlab.gaoinnovations.gov/WangC1/gao_da.git@v1.0.0
-git+https://gitlab.gaoinnovations.gov/WangC1/gao_st.git@v1.0.5
+git+https://github.com/WangC1/gao_da.git@v1.0.0
+git+https://github.com/WangC1/gao_st.git@v1.0.5

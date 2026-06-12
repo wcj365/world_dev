c0df7d19bf58094e0f1f8055d0983f1dda71d297
Dr. Chaojie Wang <wcj365@gmail.com>
2026-06-12 08:57:39 -0400

    Remove unused imports from world_dev.py
    Removed unused imports from ideal_util.common.

--- app/world_dev.py
+++ app/world_dev.py
@@ -7,8 +7,6 @@ from pyjstat import pyjstat
 import pandas as pd
 import streamlit as st
 
-from ideal_util.common import ideal_config, ideal_ui, ideal_server
-
 url_topics ="https://api.worldbank.org/v2/en/topic?format=json"
 url_indicators = "https://api.worldbank.org/v2/en/topic/{}/indicator?format=json&page={}"
 url_indicator = "https://api.worldbank.org/v2/en/indicator/{}?format=json"
